7657dcf9ec8f9003570d4ce0ef0fe1274e527897
Gregorysmel <63446621+Gregorysmel@users.noreply.github.com>
2022-02-19 13:54:59 +0200

    Update menu.php

--- menu.php
+++ menu.php
@@ -3,7 +3,7 @@
 <link rel = "stylesheet" href="css/css1.css">
 <div style="text-align:left">
     <a href="Home.php">
-	<img src="Images/logo2.png" width="20%" height="15%" >
+	<img src="Images/logo.png" width="20%" height="15%" >
     </a>
 </div>
 <body>
@@ -11,14 +11,15 @@
 <div class = "menu">
   <a  href="Home.php">Home</a>
   <div class="dropdown">
-    <a  href="">Καθημερινές</a>
-    <a  href="">Κοινωνικές</a>
-    <a  href="">Νομοθετικές</a>
-    <a  href="">Εμπορικές</a>
-    <a  href="">Εκπαιδευτικές</a>
+    <a  href="Kathimerina.php">Καθημερινά</a>
+    <a  href="Koinonia.php">Κοινωνία</a>
+    <a  href="Nomothetikes.php">Νομοθεσία</a>
+    <a  href="Oikonomia.php">Οικονομία</a>
+    <a  href="Ekpaideusi.php">Εκπαίδευση</a>
   </div>
   <div class="menu-right">
-     <a href=""> <img src="Images/bell.png" width = 25px></a>
+     <a href=""> <img src="Images/bookmark2.png" width = 18px></a>
+     <a href=""> <img src="Images/bell3.png" width = 25px></a>
      <a href="">Προφιλ</a>
    </div>  
 </div>
